7ff61624aa431d7be3f782ec6823240a769bf6ed
FKSI <ky.francois.3@gmail.com>
2015-02-21 14:04:03 +0100

    Double channel feature
    Add feature to handle both normal and photo catergory messages

--- public/js/client.js
+++ public/js/client.js
@@ -1,45 +1,52 @@
 var app = angular.module('clientApp', ['ngCookies']);
 
-app.controller('clientCtrl', ['$scope','$filter','$cookieStore', function ($scope,$filter,$cookieStore) {
+app.controller('clientCtrl', ['$scope', '$filter', '$cookieStore', function ($scope, $filter, $cookieStore) {
 	/********** Variables init **********/
 	var socket = io.connect();
 	var _msg = {
+		_category: '',
 		_type: '',
 		_txtContent: '',
 		_imgContent: ''
-	}
+	};
 	var MSG_TYPE = {
 		0: "image",
 		1: "medias",
 		2: "text"
-	}
+	};
+	var MSG_CAT = {
+		0: "normal",
+		1: "photoHunt"
+	};
 	$scope.user = {
 		login: ''
 	}
 	$scope.isLogin = true;
 	$scope.isLoadImageFinish = false;
 	$scope.msg = {
+		_category: '',
 		_type: '',
 		_txtContent: '',
 		_imgContent: ''
 	}
 	$scope.msg._txtContent = '';
 	$scope.previewImage = '';
-	
-	if($cookieStore.get('cookedNickname') != ''){
+	$scope.isPH = false;
+
+	if ($cookieStore.get('cookedNickname') != '') {
 		$scope.user.login = $cookieStore.get('cookedNickname');
-	}else{
+	} else {
 		$scope.user.login = '';
 	}
-	
-	
+
+
 	/**
-	*
-	* Save the uploaded image to an AngularJS scope variable
-	* Load the uploaded image as a source of the preview image DOM element
-	* Update the UI accordingly
-	*
-	**/
+	 *
+	 * Save the uploaded image to an AngularJS scope variable
+	 * Load the uploaded image as a source of the preview image DOM element
+	 * Update the UI accordingly
+	 *
+	 **/
 	function previewFile() {
 		if (window.File && window.FileReader && window.FileList && window.Blob) {
 			// Get the img DOM element
@@ -73,21 +80,20 @@ app.controller('clientCtrl', ['$scope','$filter','$cookieStore', function ($scop
 
 
 	/**
-	*
-	* Handle the login form
-	*
-	**/
+	 *
+	 * Handle the login form
+	 *
+	 **/
 	$scope.validateLoginForm = function () {
-		var nick = $scope.user.login;
+		var nick = $filter('uppercase')($scope.user.login);
 		if (nick != undefined && nick != '') {
 			$cookieStore.put('cookedNickname', nick);
-			socket.emit('choose nickname', $filter('uppercase')($scope.user.login), function (err) {
+			socket.emit('choose nickname',nick , function (err) {
 				if (err) {
 					//TODO Display in the UI
 					console.log("Login error %O", err);
 					$scope.isLogin = true;
 				} else {
-					console.log("Login success");
 					$scope.isLogin = false;
 				}
 				$scope.$digest();
@@ -95,32 +101,39 @@ app.controller('clientCtrl', ['$scope','$filter','$cookieStore', function ($scop
 
 		}
 	}
-	
+
 	/**
-	*
-	* Wrapper to hide ugly input[type=file] native button
-	*
-	**/
+	 *
+	 * Wrapper to hide ugly input[type=file] native button
+	 *
+	 **/
 	$scope.uploadFile = function () {
 		$('#filesToUpload').click();
 	}
-	
-	
+
+
 	/**
-	*
-	* Handle the message sending process
-	* At the end sends the correct defined object
-	* 
-	* Set the type of the message according to the content
-	*
-	**/
+	 *
+	 * Handle the message sending process
+	 * At the end sends the correct defined object
+	 *
+	 * Set the type of the message according to the content
+	 *
+	 **/
 	$scope.sendMessage = function () {
 		var txtContent = '';
 		var imgContent = '';
 		txtContent = $scope.msg._txtContent;
 		imgContent = $scope.previewImage;
 
+		// Clone _msg structure to a temporary variable
 		var temp = jQuery.extend(true, {}, _msg);
+
+		if (!$scope.isPH) {
+			temp._category = MSG_CAT[0];
+		} else {
+			temp._category = MSG_CAT[1];
+		}
 
 		if (txtContent == "" && (imgContent != undefined || imgContent != "")) {
 			// Image only type message
@@ -136,7 +149,8 @@ app.controller('clientCtrl', ['$scope','$filter','$cookieStore', function ($scop
 			temp._txtContent = txtContent;
 			temp._imgContent = imgContent;
 		}
-		
+
+
 		// Send the message
 		socket.emit('message', temp);
 
--- public/js/master.js
+++ public/js/master.js
@@ -1,45 +1,67 @@
 var app = angular.module('masterApp', []);
 
-app.controller('masterCtrl', ['$scope', function($scope){
+app.controller('masterCtrl', ['$scope', function ($scope) {
 	var socket = io.connect();
-	var MSG_TYPE = {0:"image", 1:"medias", 2:"text"};
-	$scope.msg = {
+	var MSG_TYPE = {
+		0: "image",
+		1: "medias",
+		2: "text"
+	};
+	var MSG_CAT = {
+		0: "normal",
+		1: "photoHunt"
+	};
+
+	$scope.normalMsg = {
+		_category: '',
 		_type: '',
 		_txtContent: '',
 		_imgContent: '',
-		_nickName:''
+		_nickName: '',
+		_isImg: false,
+		_isEmpty: true
 	};
-	$scope.displayNormalMsg = false;
-	$scope.displayNormalImg = false;
 
-	
-	function displayMsg(data){
-		$scope.msg._nickName = data.nick;
-		switch(data.type){
-			case MSG_TYPE[0]:
-				$scope.msg._imgContent = data.imgContent;
-				console.log($scope.msg._imgContent)
-				$scope.displayNormalImg = true;
-				break;
-			case MSG_TYPE[1]:
-						
-				$scope.msg._txtContent = data.txtContent;
-				$scope.msg._imgContent = data.imgContent;
-				console.log($scope.msg._imgContent)
-				$scope.displayNormalImg = true;
-				break;
-			case MSG_TYPE[2]:
-				$scope.msg._txtContent = data.txtContent;
-				break;
+	$scope.photoHuntMsg = {
+		_category: '',
+		_type: '',
+		_txtContent: '',
+		_imgContent: '',
+		_nickName: '',
+		_isImg: false,
+		_isEmpty: true
+	};
+
+	function displayMsg(data, msgCategoryObject) {
+		msgCategoryObject._nickName = data.nick;
+		switch (data.type) {
+		case MSG_TYPE[0]:
+			msgCategoryObject._imgContent = data.imgContent;
+			msgCategoryObject._isImg = true;
+			break;
+		case MSG_TYPE[1]:
+			msgCategoryObject._txtContent = data.txtContent;
+			msgCategoryObject._imgContent = data.imgContent;
+			msgCategoryObject._isImg = true;
+			break;
+		case MSG_TYPE[2]:
+			if (msgCategoryObject._imgContent != '') {
+				msgCategoryObject._imgContent = ''
+				msgCategoryObject._isImg = false;
+			}
+			msgCategoryObject._txtContent = data.txtContent;
+			break;
 		}
-    }
-	
-	
-	socket.on('message', function(data){
-    	displayMsg(data);
-		$scope.displayNormalMsg = true;
+		msgCategoryObject._isEmpty = false;
 		$scope.$digest();
-    });
+	}
+
+	socket.on('message', function (data) {
+		if (data.category == MSG_CAT[0]) {
+			displayMsg(data, $scope.normalMsg);
+		} else if (data.category == MSG_CAT[1]) {
+			displayMsg(data, $scope.photoHuntMsg);
+		}
+	});
 
 }]);
-
